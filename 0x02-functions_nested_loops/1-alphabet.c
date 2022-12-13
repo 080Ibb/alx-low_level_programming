0ad1751adea305ad7abcecfa7bee3cf0728b56a3
080Ibb <salmanibrahima02@gmail.com>
2022-12-13 09:42:28 -0800

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,17 +2,13 @@
 
 /**
  * main - prints alphabets in lowercase
- * Return: Always 0
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char lc;
 
 	for (lc = 'a'; lc <= 'z'; lc++)
-	{
 		_putchar(lc);
-	}
-	_putchar('\n');
 
-		return (0);
+	_putchar('\n');
 }
